23523abf894f79b5b344e738dbe5d5e5b8cb44b0
Damien Neil <neild@misago.org>
1999-02-22 22:10:35 +0000

    Added help.

--- TLily/User.pm
+++ TLily/User.pm
@@ -45,6 +45,19 @@ sub new {
 				call  => sub { $self->input_handler(@_); });
 
 	bless $self, $class;
+
+	$self->command_r(help => \&help_command);
+	$self->shelp_r(help => "Display help pages.");
+	$self->help_r(commands => \&command_help);
+	$self->help_r(help => '
+Welcome to Tigerlily!
+
+Tigerlily is a client for the lily CMC, written entirely in 100% pure Perl.
+
+For a list of commands, try "/help commands".
+');
+
+	return $self;
 }
 
 
@@ -78,10 +91,26 @@ sub command_u {
 }
 
 
+sub shelp_r {
+	my($self, $command, $help) = @_;
+	$self->{shelp}->{$command} = $help;
+}
+
+
+sub help_r {
+	my($self, $topic, $help) = @_;
+	if (!ref($help)) {
+		# Eliminate all leading newlines, and enforce only one trailing
+		$help =~ s/^\n*//s; $help =~ s/\n*$/\n/s;
+	}
+	$self->{help}->{$topic} = $help;
+}
+
+
 sub input_handler {
 	my($self, $e, $h) = @_;
 
-	return unless ($e->{text} =~ /^\s*%(\w+)\s*(.*)/);
+	return unless ($e->{text} =~ /^\s*%(\w+)\s*(.*?)\s*$/);
 	my $command = $self->{abbrevs}->{$1};
 
 	unless ($command) {
@@ -89,8 +118,45 @@ sub input_handler {
 		return 1;
 	}
 
-	$self->{commands}->{$command}->($e->{ui}, $2);
+	$self->{commands}->{$command}->($self, $e->{ui}, $2);
 	return 1;
+}
+
+
+sub command_help {
+	my($self, $ui, $arg) = @_;
+
+	$ui->indent("? ");
+	$ui->print("Tigerlily client commands:\n");
+
+	my $c;
+	foreach $c (sort keys %{$self->{commands}}) {
+		$ui->printf("  %%%-15s", $c);
+		$ui->print($self->{shelp}->{$c}) if ($self->{shelp}->{$c});
+		$ui->print("\n");
+	}
+
+	$ui->indent("");
+}
+
+
+sub help_command {
+	my($self, $ui, $arg) = @_;
+	$arg = "help" if ($arg eq "");
+
+	unless ($self->{help}->{$arg}) {
+		$ui->print("(there is no help on \"$arg\")\n");
+	}
+
+	elsif (ref($self->{help}->{$arg}) eq "CODE") {
+		$self->{help}->{$arg}->($self, $ui, $arg);
+	}
+
+	else {
+		$ui->indent("? ");
+		$ui->print($self->{help}->{$arg});
+		$ui->indent("");
+	}
 }
 
 
