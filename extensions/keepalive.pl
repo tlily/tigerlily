f67d4c622ed5123ef0143b0a1fbed3d16320de0b
Josh Wilmes <josh@hitchhiker.org>
1999-03-01 21:09:09 +0000

    added

---
+++ extensions/keepalive.pl
@@ -0,0 +1,51 @@
+# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/keepalive.pl,v 1.1 1999/03/01 21:09:09 josh Exp $
+#
+# keepalive -- periodically ping the server, just to verify our connection
+#              is still there.
+#
+
+shelp_r("keepalive" => "Send periodic pings to the server.");
+help_r("keepalive",
+'The keepalive extension is useful for maintaining a connection to the
+server on links which drop after a period of inactivity.  (Such as when
+sitting behind a firewall doing NAT.)  Keepalive will send a "/why" to
+the server every few minutes.  There are two configuration variables:
+
+    $keepalive_interval - Specifies the frequency (in seconds) to send pings.
+    $keepalive_debug    - Set this to be notified when a ping is sent.');
+
+my $pinging = 0;
+
+sub keepalive($) {
+    my($handler) = @_;
+
+    my $ui = ui_name();
+
+    $ui->printf("(keepalive)\n") if ($config{keepalive_debug});
+    if ($pinging == 1) {
+	$ui->print("(server not responding)\n");
+	$pinging = 2;
+    } elsif ($pinging == 0) {
+	$pinging = 1;
+	cmd_process("/why", sub {
+			my($event) = @_;
+			$event->{NOTIFY} = 0;
+			return unless ($event->{type} eq 'endcmd');
+			if ($pinging == 2) {
+			    $ui->print("(server is responding again)\n");
+			}
+			$pinging = 0;
+			return;
+		    });
+    }
+    
+    return 0;
+}
+
+
+if ($config{keepalive_interval} <= 0) {
+    $config{keepalive_interval} = 600;
+}
+
+TLily::Event::time_r(interval => $config{keepalive_interval},
+		     call => \&keepalive);
