8e7bb259e9bc8b63dd2996d3370a11fb4e6cbfdf
Josh Wilmes <josh@hitchhiker.org>
1999-03-16 01:14:55 +0000

    Assimilated look into spellcheck.pl

--- extensions/spellcheck.pl
+++ extensions/spellcheck.pl
@@ -105,6 +105,9 @@ sub spellcheck_cmd {
 sub load {    
     command_r("spellcheck" => \&spellcheck_cmd);
 
+    TLily::UI::command_r("look" => \&look_cmd);
+    TLily::UI::bind('C-g' => "look");
+
     foreach (qw(i a about an and are as at by for from in is of on or
 		the to with ok foo bar baz perl tlily)) {
 	$stop_list{$_}=1;
@@ -128,4 +131,56 @@ NOTE: This extension requires that the \"look\" program be installed and
 
 }
 
+sub look_cmd {
+    my($ui, $command, $key) = @_;
+    my($pos, $line) = $ui->get_input;
+    my $ui_cols = 79;
 
+    # First get the portion of the line from the beginning to the
+    # character just before the cursor.
+    $a = substr($line, 0, $pos);
+    # Just keep the alphabetic characters at the end (if any).
+    $a =~ s/.*?([A-Za-z]*)$/$1/;
+
+    # The rest of the line, from the cursor to the end.
+    $b = substr($line, $pos);
+    # Just keep the alphabetic characters at the beginning (if any).
+    $b =~ s/[^A-Za-z].*//;
+
+    my $word = $a . $b;
+    return if ($word eq '');
+
+    my @res = `look $word`;
+    chomp(@res);
+
+    if (@res == 0) {
+	$ui->print("(\"$word\" not found in dictionary)\n");
+    } elsif (@res == 1 || grep m/^$word$/i, @res) {
+	$ui->print("(\"$word\" is spelled correctly)\n");
+    } else {
+	$ui->print("(The following possible words were found:)\n");
+
+	my $clen = 0;
+	foreach (@res) { $clen = length $_ if (length $_ > $clen); }
+	$clen += 2;
+
+	my $cols = int($ui_cols / $clen);
+	my $rows = int(@res / $cols);
+	$rows++ if (@res % $cols);
+
+	$rows = 5 if ($rows > 5);
+
+	my $i;
+	for ($i = 0; $i < $rows; $i++) {
+	    $ui->print(sprintf("%-${clen}s" x $cols,
+			      map{$res[$i+$rows*$_]} 0..$cols));
+	    $ui->print("\n");
+	}
+
+	if (@res > $rows * $cols) {
+	    $ui->print("(" . (@res - ($rows * $cols)) . " more entries follow)\n");
+	}
+    }
+
+    return;
+}
