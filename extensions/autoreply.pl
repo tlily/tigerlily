e36e2015a6362fa66fe92e6d11a4aa0f455b62dc
Damien Neil <neild@misago.org>
1999-03-16 20:04:24 +0000

    Autoreply.

---
+++ extensions/autoreply.pl
@@ -0,0 +1,91 @@
+# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/autoreply.pl,v 1.1 1999/03/16 20:04:24 neild Exp $ 
+
+use strict;
+
+
+my $autoreply_help = "
+Usage: %autoreply I'm not here right now, call me at my office, x7777.
+       %autoreply |/usr/games/fortune -o
+       %autoreply off
+       %autoreply
+
+Sends an automated reply when private messages are received.
+";
+
+# Not currently implemented.
+my $autoreply_status='';
+
+my $reply = "";
+my %last_reply;
+my $send_count = 0;
+
+sub autoreply_event {
+    my($event,$handler) = @_;
+
+    my $ui = ui_name("main");
+
+    return if ($event->{VALUE} =~ /^\[automated reply\]/);
+
+    my $from=$event->{SOURCE};
+    $from=~s/\s/_/g;
+    if ($reply) {
+        if (!$last_reply{$from} || (time()-$last_reply{$from} > 30)) {
+	    $last_reply{$from}=time();
+	    my $r;
+	    if ($reply =~ /[\|\!](.*)/) {
+		$r=`$1`;
+		$r=~s/[\n\r\s]/ /g;
+	    } else {
+		$r=$reply;
+            }
+	    $ui->print("(sending automated reply to $from: \"$r\")\n");
+	    $send_count++;
+	    $ui->set(autoreply => "(autoreply $send_count)");
+	    $event->{server}->sendln("$from:[automated reply] $r");
+        } else {
+	    $last_reply{$from}=time();
+	}
+    }
+
+    return 0;
+}
+event_r(type => 'private',
+	call => \&autoreply_event);
+
+sub autoreply_cmd {
+    my($ui, $args) = @_;
+    if ($args eq "") {
+	if ($reply) {
+	    $ui->print("(current automated reply is \"$reply\")\n");
+	} else {
+	    $ui->print("(automated reply currently disabled)\n");
+	}
+    } elsif ($args eq "off") {
+	$reply="";
+	$ui->print("(disabling automated reply to private messages)\n");
+	$send_count=0;
+	$ui->set(autoreply => undef);
+    } else {
+	$send_count=0;
+	$reply=$args;
+	$ui->print("(will send automated reply to private messages)\n");
+	$ui->set(autoreply => "(autoreply $send_count)");
+    }
+}
+command_r(autoreply => \&autoreply_cmd);
+shelp_r(autoreply => "send a canned reply to private sends");
+help_r('autoreply', $autoreply_help);
+
+
+sub unload {
+    my $ui = ui_name("main");
+    $ui->set(autoreply => undef);
+}
+
+sub load {
+    my $ui = ui_name("main");
+    $ui->define(autoreply => 'right');
+    return;
+}
+
+1;
