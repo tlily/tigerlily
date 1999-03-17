92ede574a360b8d61042972ba2ffeee5abaa2be2
Josh Wilmes <josh@hitchhiker.org>
1999-03-17 02:21:33 +0000

    Adding.  I think this extension is cool :)

---
+++ extensions/alias.pl
@@ -0,0 +1,81 @@
+use strict;
+
+my %alias;
+
+sub load {
+    event_r(type  => "user_input",
+	    order => "before",
+	    call  => \&aliaser);
+    command_r(alias => \&alias_cmd);
+
+    shelp_r(alias => "Define client aliases");
+    help_r(alias => "%alias <command> <newcommand>
+%alias clear <command>
+%alias list
+
+Supports the following special characters in \"newcommand]\":
+\$1 .. \$9  arguments to command
+\$*        all arguments to command
+\\n        command separator
+
+Examples:
+
+%alias hi bob;hi there\\njim;I hate you!
+%alias inbeener /who beener \$*
+");
+}
+
+sub alias_cmd {
+    my ($ui,$args) = @_;
+    
+    if (! length($args) || $args eq "list") {
+	if (scalar keys %alias) {
+	    $ui->print("The following aliases are defined:\n");
+	    foreach (sort keys %alias) {
+		$ui->print("$_: $alias{$_}\n");
+	    }
+	} else {
+	    $ui->print("(no aliases are currently defined)\n");
+	}
+    }
+    
+    my ($key,$val) = ($args =~ /(\S+)\s+(.*)/);
+    
+    if ($key eq "clear") {
+	undef $alias{$val};
+    }
+
+    if ($key =~ /\S/ and $val =~ /\S/) {
+	$alias{$key}=$val;
+    }
+}
+
+sub aliaser {
+    my($e, $h) = @_;
+    my $server = server_name();
+    
+    if ($e->{text} =~ /^%(\S+)\s*(.*)/) {	
+	my $newstr = $alias{$1};
+	my $args = $2;
+	my @args = ($1, (split /\s+/,$2));
+	if ($newstr) {
+	    for (0..9) {
+		$newstr =~ s/\$$_/$args[$_]/g;
+	    }
+	    $newstr =~ s/\$\*/$args/g;
+	    if ($newstr =~ /\\n/) {
+		my @rest;
+		($newstr,@rest) = split /\\n/,$newstr;
+		foreach (@rest) {
+		    TLily::Event::send({type => 'user_input',
+					ui   => $e->{ui},
+					text => "$_\n"});		    
+		}
+	    }
+	    $e->{text} = $newstr;
+	}
+    }
+
+    return 0;
+}
+
