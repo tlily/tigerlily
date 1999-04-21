6ceb2c9dc36cb4df0e3d54d1d98f1a94fd9fe2f7
Damien Neil <neild@misago.org>
1999-04-21 20:09:58 +0000

    Added help.

--- extensions/paste.pl
+++ extensions/paste.pl
@@ -1,5 +1,5 @@
 # -*- Perl -*-
-# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/paste.pl,v 1.2 1999/03/23 08:33:53 josh Exp $
+# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/paste.pl,v 1.3 1999/04/21 20:09:58 neild Exp $
 
 use strict;
 
@@ -26,9 +26,19 @@ sub toggle_paste_mode {
     }
 }
 
+my $paste_help = "
+Sometimes you want to paste several lines of text into a send.  Pasting
+each line one at a time is tedious, and prone to error.  (What happens
+if you accidentally paste a newline?)  Paste mode provides a better way.
 
-sub load {
-    TLily::UI::command_r("paste-mode"        => \&paste_mode);
-    TLily::UI::command_r("toggle-paste-mode" => \&toggle_paste_mode);
-    TLily::UI::bind("M-p" => "toggle-paste-mode");
-}
+When paste mode is enabled (it can be toggled with the toggle-paste-mode
+key, bound to M-p by default), newlines are translated into spaces.  To
+help with occasions when several lines of text are indented, any spaces
+following a newline are not entered.
+";
+
+TLily::UI::command_r("paste-mode"        => \&paste_mode);
+TLily::UI::command_r("toggle-paste-mode" => \&toggle_paste_mode);
+TLily::UI::bind("M-p" => "toggle-paste-mode");
+TLily::User::shelp_r("paste" => "Pasting multi-line text.", "concepts");
+TLily::User::help_r("paste" => $paste_help);
