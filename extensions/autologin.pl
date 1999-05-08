023aefca7728ee6bc8271b51c68aacfa62ef31f8
Josh Wilmes <josh@hitchhiker.org>
1999-05-08 04:36:00 +0000

    Moved help.

--- extensions/autologin.pl
+++ extensions/autologin.pl
@@ -1,5 +1,5 @@
 # -*- Perl -*-
-# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/autologin.pl,v 1.7 1999/04/27 18:13:07 neild Exp $
+# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/autologin.pl,v 1.8 1999/05/08 04:36:00 josh Exp $
 
 #
 # Handle autologins.
@@ -12,7 +12,7 @@ my @files = ("$ENV{HOME}/.lily/tlily/autologin",
 	     "$ENV{HOME}/.lily/lclient/autologin");
 unshift @files, $config{'autologin_file'} if ($config{'autologin_file'});
 
-shelp_r("autologin", "Module for automating the login process.");
+shelp_r("autologin", "Module for automating the login process.", "concepts");
 help_r("autologin", 
 "Reads files containing lines of the format: <green>alias host port login passwd</green> in order to automate your login process to the specified server.  Unlike lclient, all fields must be present or the line will be ignored. (FIXME!)
 Config options for autologin:
