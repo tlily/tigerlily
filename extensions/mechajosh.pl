7493c5665b18ad073ae08b483696498d19eca171
Josh Wilmes <josh@hitchhiker.org>
2000-01-13 03:19:58 +0000

    fixed urls

--- extensions/mechajosh.pl
+++ extensions/mechajosh.pl
@@ -8,7 +8,7 @@ bot_r(match => "answer",
 
 bot_r(match => "excuse",
       respond => sub { 
-	  $exc=getquote("http://www.cae.wisc.edu/cgi-bin/ballard/bofhserver.pl");
+	  $exc=getquote("http://cgi.cs.wisc.edu/scripts/ballard/bofhserver.pl");
 	  $exc =~ s/The cause of the problem is/Automatic Excuse/;		
 	  if ($exc =~ /\S/) {
 	      return($exc);		
@@ -20,9 +20,10 @@ bot_r(match => "excuse",
 
 bot_r(match => "surreal|weird|compliment",
       respond => sub {
-	  $comp=getquote("http://pharmdec.wustl.edu/cgi-bin/jardin_scripts/SCG");		
+	  $comp=getquote("http://www.madsci.org/cgi-bin/cgiwrap/~lynn/jardin/SCG");
 	  if ($comp =~ /\S/) {
-	      return($comp);		
+	      s/^\s*//;
+	      return($comp);
 	  } else {
 	      return(undef);
 	  }
