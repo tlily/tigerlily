cb0fa584eb8fead969858528d8a878568c436eb0
Matthew J. Ryan <mjr@centauri.org>
2000-09-09 22:21:10 +0000

    Cleaned up xmms.pl, added ability to play URLs.

--- extensions/xmms.pl
+++ extensions/xmms.pl
@@ -1,16 +1,16 @@
 # -*- Perl -*-
-# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/xmms.pl,v 1.1 2000/09/08 02:49:31 coke Exp $
+# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/xmms.pl,v 1.2 2000/09/09 22:21:10 mjr Exp $
 
 use strict;
+use Xmms::Remote;
+eval 'use Xmms::Remote;';
+if ($@) {
+    die "xmms.pl requires Xmms::Remote to be installed.\n";
+}
 
 # Author:   Will "Coke" Coleda
 # Created:  August, 2000
 # Purpose:  Control xmms from tigerlily.
-# Danger:   Hackery abounds. I cannot even begin to describe how
-#           bletcherous this extension is.
-# Requires: that you have the Xmms modules installed, and that perl be
-#           in your path.
-# BUGS:     figure out how to call Xmms IN THIS INSTANCE OF PERL. ugh.
 
 command_r('xmms', \&xmms_cmd);
 shelp_r('xmms', "Control xmms from lily");
@@ -26,8 +26,7 @@ song returns the title of the currently playing song.
 playlist returns the entire playlist. be careful.
 ");
 
-my $command = '`perl -MXmms::Remote -e \'\$r=Xmms::Remote->new; \$t = \$r->%s; if (ref \$t eq "ARRAY") {\$t = join("\n", @\$t)} print \$t;\'`';
-
+my $r;
 
 my %simple = (
 	stop => "stop",
@@ -40,34 +39,50 @@ my %simple = (
 sub xmms_cmd {
     my ($ui,$cmd) = @_;
 
+    unless ($r->is_running()) {
+	$ui->print("(Xmms is not running)\n");
+        return;
+    }
+
     if (exists $simple{$cmd}) {
-      eval sprintf $command, $simple{$cmd};
+        $cmd = $simple{$cmd};
+        my $res = $r->$cmd();
+        $ui->print("($res)\n") if $res;
+    } elsif ($cmd =~ m{play ((?:http|file)://.+)}i) {
+        my $res = $r->playlist_add_url($1);
+        $ui->print("($res)\n") if $res;
+        $r->set_playlist_pos($r->get_playlist_length());
+        $r->play();
     } elsif ($cmd eq "vol") {
-        my $vol = eval sprintf $command, "get_main_volume" ;
+        my $vol = $r->get_main_volume();
 	$ui->print("(vol is approximately $vol)\n");
     } elsif ($cmd =~ /vol (\d+)/) {
-	eval sprintf $command, "set_main_volume($1)";
+        $r->set_main_volume($1);
+	$ui->print("(vol is approximately $1)\n");
     } elsif ($cmd =~ /vol ([+-])(\d+)/) {
-        my $vol = eval sprintf $command, "get_main_volume" ;
+        my $vol = $r->get_main_volume();
  	if ($1 eq "+") {
 		$vol += $2;
 	} else {
 		$vol -= $2;
 	}
-	eval sprintf $command, "set_main_volume($vol)";
+        $r->set_main_volume($vol);
 	$ui->print("(vol is approximately $vol)\n");
     } elsif ($cmd eq "song") {
-        my $pl = eval sprintf $command, "get_playlist_titles" ;
-        my $pos = eval sprintf $command, "get_playlist_pos" ;
-	$ui->print("(" . (split(/\n/,$pl))[$pos] . ")\n" );
+        my $title = $r->get_playlist_title();
+	$ui->print("($title)\n");
     } elsif ($cmd eq "playlist") {
-        my $pl = eval sprintf $command, "get_playlist_titles" ;
-	foreach my $song (split(/\n/,$pl)) {
+        my $pl = $r->get_playlist_titles();
+	foreach my $song (@{$pl}) {
 		$ui->print("(" . $song . ")\n" );
 	}
     } else {
 	$ui->print("(Invalid subcommand. see %help xmms)\n");
     }
+}
+
+sub load {
+  $r = Xmms::Remote->new;
 }
 
 1;
