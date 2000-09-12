8438fe4798d361922295cee2c77b5577a0207960
Will Coleda <will@coleda.com>
2000-09-12 04:04:18 +0000

    Modified to use config vars instead of local hackish things.

--- extensions/stock.pl
+++ extensions/stock.pl
@@ -1,5 +1,5 @@
 # -*- Perl -*-
-# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/stock.pl,v 1.3 2000/09/11 21:15:52 coke Exp $
+# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/stock.pl,v 1.4 2000/09/12 04:04:18 coke Exp $
 
 use strict;
 
@@ -9,41 +9,49 @@ use strict;
 require LWP::UserAgent;
 my $ua = new LWP::UserAgent;
 
-my @timer_ids;
+my (%timer, $timer_id);
 my $info = "STOCK";
-my $ticker = "";
-my $freq = 300;
+$config{stock_symbol}="EXDS" if (!exists $config{stock_symbol});
+$config{stock_freq}=300 if (!exists $config{stock_freq});
 
 command_r('stock', \&stock_cmd);
+shelp_r('stock_symbol', "Display this ticker symbol", "variables");
+shelp_r('stock_freq', "Frequency in seconds to update ticker symbol","variables");
 shelp_r('stock', "Show stock ticker updates in your status bar");
-help_r( 'stock',"%stock stop                   stop ticker,
-%stock <TICKER SYMBOL list>   display oneshot update for stocks.
-%stock -t <TICKER SYMBOL>     start tracking stock (default is $ticker)
-%stock -f <frequency>         frequency in seconds to update statusbar
-                              (defaults to $freq seconds)
-%stock -l                     show current settings.
+help_r( 'stock',"%stock <TICKER SYMBOL list>   display oneshot update for stocks.
+
+There are two config variables: stock_symbol and stock_freq. The former
+controls the stock to be queried for the command line update and
+the latter controls the frequency.
+
+To force a change before the timer would go off, simply reload the extension.
 ");
+
+#
+# Once you can remove a config callback, we can prolly ditch start/stop
+#
+
 
 #
 # There's got to be a module to do this for me. =-)
 #
 sub cleanHTML {
 
-  $a = join(" ",@_);
+	$a = join(" ",@_);
 
-  $a =~ s/\n/ /;
-  $a =~ s/<[^>]*>/ /g;
-  $a =~ s/&lt;/</gi;
-  $a =~ s/&gt;/>/gi;
-  $a =~ s/&amp;/&/gi;
-  $a =~ s/&#46;/./g;
-  $a =~ s/&quot;/"/ig;
-  $a =~ s/&nbsp;/ /ig;
-  $a =~ s/&uuml;/u"/ig;
-  $a =~ s/\s+/ /g;
-  $a =~ s/^\s+//;
+	$a =~ s/\n/ /;
+	$a =~ s/<[^>]*>/ /g;
+	$a =~ s/&lt;/</gi;
+	$a =~ s/&gt;/>/gi;
+	$a =~ s/&amp;/&/gi;
+	$a =~ s/&#46;/./g;
+	$a =~ s/&quot;/"/ig;
+	$a =~ s/&nbsp;/ /ig;
+	$a =~ s/&uuml;/u"/ig;
+	$a =~ s/\s+/ /g;
+	$a =~ s/^\s+//;
 
-  return $a;
+	return $a;
 }
 
 #
@@ -65,13 +73,13 @@ sub disp_stock {
 	my @chunks = ($response->content =~ /^<td nowrap align=left>.*/mg);
 
 	foreach (@chunks) {
-                my $retval="";
+		my $retval="";
 		my ($time,$value,$frac,$perc,$volume) = (split(/<\/td>/,$_,))[1..5];
 
 		if (/No such ticker symbol/) {
 			$ui->print("($stock[$cnt]: Oops. No such ticker symbol. Try stock lookup.)\n");
 		} else {
-                        $retval = "($stock[$cnt]: Last $time, $value: Change $frac ($perc): Vol $volume)";
+			$retval = "($stock[$cnt]: Last $time, $value: Change $frac ($perc): Vol $volume)";
 			$retval = cleanHTML($retval);
 			$retval =~ s:(\d) / (\d):$1/$2:g;
 			$retval =~ s:\( :(:g;
@@ -107,7 +115,7 @@ sub track_stock {
 	if ($chunks[0] =~ /No such ticker symbol/) {
 		return "$stock: bad symbol";
 	} else {
-        	my $retval="";
+		my $retval="";
 		$retval = "$stock: $value ($frac)";
 		$retval = cleanHTML($retval);
 		$retval =~ s:(\d) / (\d):$1/$2:g;
@@ -123,60 +131,33 @@ sub track_stock {
 #
 
 sub unload {
-    foreach (@timer_ids) {
-			TLily::Event::time_u($_);
-    }
-    @timer_ids = ();
+	TLily::UI->name("main")->set(stock => "");
+	TLily::Event::time_u($timer_id);
 }
 
 
-#
-# update the stock price every $freq seconds...
-#
 
-sub setup_handler {
-    my ($ui) = @_;
 
-    my $timer_id = TLily::Event::time_r(interval => $freq,
-					call  => sub { 
-					    $info = track_stock($ticker);
-					    $ui->set(stock => $info);
-					});
-    push @timer_ids, $timer_id;
+sub start_tracker {
+	my $ui = $_[0];
+	$ui->define(stock => 'right');
+	$info = "Waiting for $config{stock_symbol}";
+	$ui->set(stock => $info);
+	$timer{interval} = $config{stock_freq};
+	$timer{call} = 
+	  sub {$ui->set(stock => track_stock($config{stock_symbol}))};
+	$timer_id = TLily::Event::time_r(\%timer);
 }
 
 #
 # Handle any new requests for information
 #
 sub stock_cmd {
-    my ($ui,$cmd) = @_;
-    
-    if ($cmd eq "stop") {
-	$ticker = "" ;
-	$info = "";
-	$ui->set(stock => $info);	
-	unload();
-    } elsif ($cmd =~ /^-t\s*(.*)/) {
-	unload();
-	$ticker = $1;
-	$ui->define(stock => 'right');
-	$info = "Waiting for $ticker";
-	$ui->set(stock => $info);
-	setup_handler($ui);
-    } elsif ($cmd =~ /^-f\s*(.*)/) {
-	unload();
-	$freq = $1;
-	setup_handler($ui);
-    } elsif ($cmd eq "-l" || $cmd !~ /\S/) {
-	if (@timer_ids) {
-	    $ui->print("(Tracking: '$ticker' at a frequency of $freq seconds.)\n");
-	} else {
-	    $ui->print("(currently not tracking any stocks)\n");
-	}
-    } else {
-	disp_stock($ui,split(/\s+/,$cmd));
-    }
+	my ($ui,$cmd) = @_;
+	disp_stock($ui,split(/[\s,]+/,$cmd));
 }
+
+start_tracker(TLily::UI->name("main"));
 
 #TRUE! They return TRUE!
 1;
