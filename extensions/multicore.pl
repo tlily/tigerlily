7ea4cd8511e4abb614b843d24b3b2963e8eb4f09
Damien Neil <neild@misago.org>
2001-07-30 18:41:17 +0000

    - Added electric-expansion on /. - Removed the initial @ from multicore commands. - Reworked the function logic a bit.

--- extensions/multicore.pl
+++ extensions/multicore.pl
@@ -1,4 +1,4 @@
-# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/multicore.pl,v 1.1 2001/07/27 18:35:29 coke Exp $
+# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/multicore.pl,v 1.2 2001/07/30 18:41:17 neild Exp $
 
 use strict;
 
@@ -9,34 +9,51 @@ TLily::Event::event_r(type  => "user_input",
 sub input_handler {
     my($e, $h) = @_;
 
-    next unless $e->{text} =~ m/^[@\*]/;
+    next unless $e->{text} =~ m/^([^@;:=\s]+)(\/.*)/;
+    my($server, $command) = ($1, $2);
 
-    my($servers); 
-    foreach (TLily::Server::find()) {
-      $servers->{$_->name()} = $_;
-    }
-
-    if ($e->{text} =~ m:^\@([A-Z_a-z]+)(/..*):) {
-      my ($server,$command) = ($1,$2);
-      chomp($command);
-
-
-      if (not exists($servers->{$server})) {
-        TLily::UI::name("main")->print("Bad server: $server: try one of ". join(", ",%$servers) . "\n");
+    if ($server ne "*") {
+      my $s = TLily::Server::find($server);
+      if (!$s) {
+	$e->{ui}->print("(could find no server to match to \"$server\")\n");
         return 1;
       }
 
-      $servers->{$server}->cmd_process($command, sub {;});
+      $s->cmd_process($command, sub {;});
 
       return 1;
 
-    } elsif ($e->{text} =~ m:^\*(/..*):) {
-      my($command) = $1;
-      foreach (keys %$servers) {
-        $servers->{$_}->cmd_process($command, sub {;});
+    } else {
+      foreach (TLily::Server::find()) {
+        $_->cmd_process($command, sub {;});
       }
       return 1;
     }
 
     return;
 }
+
+
+sub expand_slash {
+    my($ui, $command, $key) = @_;
+    my($pos, $line) = $ui->get_input;
+    my $partial = substr($line, 0, $pos);
+
+    if (length($partial) && $partial !~ m|[/@;:= ]|) {
+	my @servers =
+	  grep(/^\Q$partial\E/i, map(scalar $_->name, TLily::Server::find));
+	if (@servers == 1) {
+	    $ui->set_input(length($servers[0]) + 1,
+			   $servers[0].substr($line, $pos)."/");
+	    return;
+	}
+    }
+
+    $ui->command("insert-self", $key);
+    return;
+
+
+}
+
+TLily::UI::command_r("electric-slash" => \&expand_slash);
+TLily::UI::bind('/' => "electric-slash");
