66fbc3b897f757e9772039fc7b6802315f87ca15
Damien Neil <neild@misago.org>
2001-10-02 21:57:12 +0000

    />0|\|'t 45|<.

---
+++ extensions/leet.pl
@@ -0,0 +1,55 @@
+# -*- Perl -*-
+# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/leet.pl,v 1.1 2001/10/02 21:57:12 neild Exp $
+
+use strict;
+
+my %map = (
+	   "a" => "4",
+	   "b" => "8",
+	   "d" => "/>",
+	   "e" => "3",
+	   "h" => "|-|",
+	   "i" => "1",
+	   "k" => "|<",
+	   "l" => "l",
+	   "m" => "|\\/|",
+	   "n" => "|\|",
+	   "o" => "0",
+	   "s" => "5",
+	   "u" => "|_|",
+	   "v" => "\\/",
+	   "w" => "\\//",
+	  );
+
+sub leet_mode {
+    my($ui, $command, $key) = @_;
+
+    my($pos, $line) = $ui->get_input;
+    return unless (substr($line, 0, $pos) =~ /[;:=]/);
+
+    my $k = $map{lc($key)};
+    if (defined $k) {
+	    for my $k (split //, $k) {
+		    $ui->command("insert-self", $k);
+	    }
+	    return 1;
+    }
+    return;
+}
+
+sub toggle_leet_mode {
+    my($ui, $command) = @_;
+    #$ui->intercept_u("leet-mode") || $ui->intercept_r("leet-mode");
+    if ($ui->intercept_u("leet-mode")) {
+	    $ui->set(leet => undef);
+    } else {
+	    $ui->intercept_r("leet-mode");
+	    $ui->define(leet => 'left');
+	    $ui->set(leet => "L33T");
+    }
+    return;
+}
+
+TLily::UI::command_r("leet-mode"        => \&leet_mode);
+TLily::UI::command_r("toggle-leet-mode" => \&toggle_leet_mode);
+TLily::UI::bind("M-l" => "toggle-leet-mode");
