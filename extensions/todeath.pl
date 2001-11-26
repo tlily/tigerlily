8d218f16acbe1bb9498cb47d6428e8907ffaf221
Josh Wilmes <josh@hitchhiker.org>
2001-11-26 19:51:38 +0000

    Made configurable.

--- extensions/todeath.pl
+++ extensions/todeath.pl
@@ -1,5 +1,5 @@
 # -*- Perl -*-
-# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/todeath.pl,v 1.1 2001/11/26 02:45:37 josh Exp $
+# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/todeath.pl,v 1.2 2001/11/26 19:51:38 josh Exp $
 
 use strict;
 
@@ -10,18 +10,21 @@ todeath.pl - Fix stupid political correctness.
 =head1 DESCRIPTION
 
 When loaded, this extension will change the "(idled off the server)" message
-back to its rightful "(idled to death)".
+back to its rightful "(idled to death)" (or whatever the 'todeath_message'
+config variable is set to).
 
 =cut
 
 sub handler {
     my($event, $handler) = @_;
 
-    $event->{VALUE} =~ s/off the server/to death/g;
-    $event->{text}  =~ s/off the server/to death/g;
+    $event->{VALUE} =~ s/off the server/$config{todeath_message}/g;
+    $event->{text}  =~ s/off the server/$config{todeath_message}/g;
 
     return 0;
 }
+
+$config{todeath_message} ||= "idled to death";
 
 event_r(type  => 'disconnect',
 	call  => \&handler,
