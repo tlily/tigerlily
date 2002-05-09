57cd84627fed968a89f8130c53097d3e61084fdb
Will Coleda <will@coleda.com>
2002-05-09 19:47:56 +0000

    code refactor, added "find" subcmd

--- extensions/group.pl
+++ extensions/group.pl
@@ -1,5 +1,5 @@
 # -*- Perl -*-
-# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/group.pl,v 1.1 2002/05/09 18:57:53 coke Exp $
+# $Header: /home/mjr/tmp/tlilycvs/lily/tigerlily2/extensions/group.pl,v 1.2 2002/05/09 19:47:56 coke Exp $
 
 use strict;
 
@@ -18,35 +18,68 @@ command_r('group', \&group_cmd);
 shelp_r('group', "Pretty print group info.");
 help_r( 'group',"%group: like /group but prettier.");
 
-sub group_cmd {
-  local $config{expand_group} = 1;
+sub print_results {
+  my ($ui,$msg,%groups) = @_;
 
-  my $server = TLily::Server::active();
-  my $ui = TLily::UI::name("main");
+  if (keys %groups) {
+    $ui->print("Group      Members\n");
+    $ui->print("-----      -------\n");
 
-  my @groups;
-  foreach my $name (keys %{$server->{NAME}}) {
-    if (defined($server->{NAME}->{$name}->{MEMBERS})) {
-      #use Data::Dumper; $ui->print(Dumper($name));
-      push @groups, $name;
+    my $colwidth = 11;
+    foreach my $name (sort {lc($a) cmp lc($b)} keys %groups) {
+      $ui->print($name . " " x ($colwidth-length($name)));
+      $ui->indent(" " x $colwidth);
+      my $expand = join (", ",sort {lc($a) cmp lc($b)} keys %{$groups{$name}} );
+      $ui->print($expand);
+      $ui->indent();
+      $ui->print("\n");
     }
-  }
-
-  if (@groups) {
-	$ui->print("Group      Members\n");	
-	$ui->print("-----      -------\n");	
-  }
-  my $colwidth = 11;
-  foreach my $name (sort {lc($a) cmp lc($b)}  @groups) {
-    $ui->print($name . " " x ($colwidth-length($name)));
-    $ui->indent(" " x $colwidth);
-    my $expand = join (", ",sort {lc($a) cmp lc($b)} split(',',$server->expand_name($name)));
-    $ui->print($expand);
-    $ui->indent();
-    $ui->print("\n");
+  } else {
+    $ui->print("($msg)\n");
   }
 }
 
+sub group_cmd {
+  my ($ui) = shift;
+  my ($subcmd,@args)= split(' ',"@_");
+
+  local $config{expand_group} = 1;
+
+  my $server = TLily::Server::active();
+
+  $subcmd ||= "";
+
+  my %groups;
+  foreach my $name (keys %{$server->{NAME}}) {
+    if (defined($server->{NAME}->{$name}->{MEMBERS})) {
+      foreach my $member (split(',',$server->expand_name($name))) {
+        $groups{$name}->{lc($member)} = 1;
+      }
+    }
+  }
+  
+  if ($subcmd eq "") {
+    print_results($ui,"you have no groups defined",%groups);
+  } elsif ($subcmd eq "find") {
+      if (! @args) {
+        $ui->print("You must want to find something.\n");
+        return;
+      }
+    my %hits;
+    foreach my $name (keys %groups) {
+      foreach my $member (keys %{${groups}{$name}}) {
+        foreach my $match (@args) {
+          lc($match);
+          if ($member =~ /$match/) {
+           $hits{$name}{$member} = 1;
+          }
+        }
+      }
+    }
+
+    print_results($ui,"no matches found", %hits);
+  }
+}
 
 #TRUE! They return TRUE!
 1;
