a2223503b9bfc2679a4458e5e451d2746d84155a
Matthew J. Ryan <mjr@centauri.org>
2006-01-22 05:12:12 +0000

    * Forgot ta add TLily/Server/IRC/

---
+++ lib/TLily/Server/IRC/Driver.pm
@@ -0,0 +1,55 @@
+# -*- Perl -*-
+#    TigerLily:  A client for the lily CMC, written in Perl.
+#    Copyright (C) 2006  The TigerLily Team, <tigerlily@tlily.org>
+#                                http://www.tlily.org/tigerlily/
+#
+#  This program is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License version 2, as published
+#  by the Free Software Foundation; see the included file COPYING.
+#
+# $Id$
+
+package TLily::Server::IRC::Driver;
+
+use strict;
+
+use Carp;
+use Net::IRC;
+use TLily::Event;
+use Data::Dumper;
+
+use base qw(Net::IRC);
+
+my %tlily_io_handlers;
+
+sub addfh() {
+    my $self = shift;
+    my ($fh, $code, $flag) = @_;
+
+    my $id = TLily::Event::io_r(handle => $fh,
+                                mode => lc($flag),
+				call => \&Net::IRC::do_one_loop,
+				obj => $self);
+
+    push @{$tlily_io_handlers{fileno($fh)}}, $id;
+
+    $self->SUPER::addfh(@_);
+}
+
+sub removefh() {
+    my $self = shift;
+    my ($fh) = @_;
+
+    if (exists $tlily_io_handlers{fileno($fh)}) {
+        foreach my $id (@{$tlily_io_handlers{fileno($fh)}}) {
+            TLily::Event::io_u($id);
+	    delete $tlily_io_handlers{fileno($fh)};
+        }
+    } else {
+        warn("Could not find tlily handler for IRC filehandle.  Please report this as a bug using '%submit client', and provide details on what you were doing when it happened.  If possible, please give a recipe for reproducing the bug.");
+    }
+
+    $self->SUPER::removefh(@_);
+}
+
+1;
