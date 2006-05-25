14855df2cd7d8421363652a7f8684cdc32c07d64
Will Coleda <will@coleda.com>
2006-05-25 13:53:47 +0000

    Remove hard tabs.

--- lib/TLily/Server/SLCP.pm
+++ lib/TLily/Server/SLCP.pm
@@ -109,7 +109,7 @@ sub init () {
                 unless ! $ui;
             unless (save_deadfile($ex->{type}, $event->{server},
                                   $ex->{name}, $ex->{text})) {
-	        $ui->print("(Unable to save \"$ex->{name}\"; changes lost)\n")
+            $ui->print("(Unable to save \"$ex->{name}\"; changes lost)\n")
                     unless ! $ui;
             }
         }
@@ -161,46 +161,46 @@ sub new {
 
     # set the client name once we're %connected.
     my $sub = sub {
-	my ($e,$h) = @_;
+        my ($e,$h) = @_;
 
-	return 0 unless ($e->{server} == $self);
+        return 0 unless ($e->{server} == $self);
 
-	$self->set_client_name();
-	$self->get_user_perms();
-	TLily::Event::event_u($h->{id});
-
-	return 0;
+        $self->set_client_name();
+        $self->get_user_perms();
+        TLily::Event::event_u($h->{id});
+    
+        return 0;
     };
     TLily::Event::event_r(type => "connected",
-			  call => $sub);
-	
+                          call => $sub);
+    
     # set the client options at the first prompt.
     $sub = sub {
-	my ($e,$h) = @_;
-	return 0 unless ($e->{server} == $self);
-	my $ui = TLily::UI::name($self->{ui_name});
+        my ($e,$h) = @_;
+        return 0 unless ($e->{server} == $self);
+        my $ui = TLily::UI::name($self->{ui_name});
 
-	TLily::Event::event_u($h->{id});
+        TLily::Event::event_u($h->{id});
 
-	$self->set_client_options();
-	
-	# allow the user's input to go to the server now.
-	$self->{ALLOW_SEND} = 1;
-	
-	if (defined $self->{user}) {
-	    $ui->print("(using autologin information)\n") if ($ui);
-	    $self->send($self->{user});
-	    $self->send(" ".$self->{password}) if defined ($self->{password});
-	    $self->sendln();
-	    delete $self->{user};
-	    delete $self->{password};
-	    return 1;
-	}
+        $self->set_client_options();
+    
+        # allow the user's input to go to the server now.
+        $self->{ALLOW_SEND} = 1;
+    
+        if (defined $self->{user}) {
+            $ui->print("(using autologin information)\n") if ($ui);
+            $self->send($self->{user});
+            $self->send(" ".$self->{password}) if defined ($self->{password});
+            $self->sendln();
+            delete $self->{user};
+            delete $self->{password};
+            return 1;
+        }
 
-	return 0;
+        return 0;
     };
     TLily::Event::event_r(type => "prompt",
-			  call => $sub);
+                          call => $sub);
 
     bless $self, $class;
 }
@@ -220,8 +220,8 @@ sub command {
         foreach (@{$self->{send_buffer}}) {
             $self->sendln($_);
         }
-	undef $self->{send_buffer};
-	
+        undef $self->{send_buffer};
+    
         # Send the line on to the server.
         $self->sendln($text);
 
@@ -314,26 +314,26 @@ sub expand_name {
 
     # Check for "me".
     if (!$disc && $name eq 'me') {
-	return $userprefix . $self->user_name || 'me';
+        return $userprefix . $self->user_name || 'me';
     }
 
     # Check for a group match.
     if (!$user && !$disc && $self->{NAME}->{$name}->{MEMBERS}) {
-    	if ($config{expand_group}) {
-	    return join ',', map { $self->get_name(HANDLE => $_) }
- 	                         split /,/,$self->{NAME}->{$name}->{MEMBERS};
-	} else {
-	    return $self->{NAME}->{$name}->{NAME};
-	}
+        if ($config{expand_group}) {
+        return join ',', map { $self->get_name(HANDLE => $_) }
+                              split /,/,$self->{NAME}->{$name}->{MEMBERS};
+        } else {
+            return $self->{NAME}->{$name}->{NAME};
+        }
     }
     
     # Check for an exact match.
     if ($self->{NAME}->{$name}) {
-	if ($self->{NAME}->{$name}->{LOGIN} && !$disc) {
-	    return $userprefix . $self->{NAME}->{$name}->{NAME};
-	} elsif ($self->{NAME}->{$name}->{CREATION} && !$user) {
-	    return '-' . $self->{NAME}->{$name}->{NAME};
-	}
+        if ($self->{NAME}->{$name}->{LOGIN} && !$disc) {
+            return $userprefix . $self->{NAME}->{$name}->{NAME};
+        } elsif ($self->{NAME}->{$name}->{CREATION} && !$user) {
+            return '-' . $self->{NAME}->{$name}->{NAME};
+        }
     }
 
     return if $opts{exact};
@@ -342,56 +342,56 @@ sub expand_name {
     # Should prepend the magic ~/- as well.
 
     if (ref($config{prefer}) eq "ARRAY") {
-	my $m;
-	foreach $m (@{$config{prefer}}) {
+        my $m;
+        foreach $m (@{$config{prefer}}) {
             next unless $m =~ /^-?(.*)/;
             next unless $self->{NAME}->{$1};
 
-	    $m = lc($m);
-	    $m =~ tr/_/ /;
-	    return $m if (index($m, $name) == 0);
-	    return $m if ($m =~ /^-/ && index($m, $name) == 1);
-	}
+            $m = lc($m);
+            $m =~ tr/_/ /;
+            return $m if (index($m, $name) == 0);
+            return $m if ($m =~ /^-/ && index($m, $name) == 1);
+        }
     }
 
     my(@unames, @dnames);
     foreach (keys %{$self->{NAME}}) {
-	push @unames, $_ if ($self->{NAME}->{$_}->{LOGIN});
-	push @dnames, $_ if ($self->{NAME}->{$_}->{CREATION});
+        push @unames, $_ if ($self->{NAME}->{$_}->{LOGIN});
+        push @dnames, $_ if ($self->{NAME}->{$_}->{CREATION});
     }
 
     my @m;
     # Check for a prefix match.
     unless ($disc) {
-	@m = grep { index($_, $name) == 0 } @unames;
-	return if (@m > 1 && !wantarray);
-	return map($userprefix.$self->{NAME}->{$_}->{NAME}, @m) if (@m);
+        @m = grep { index($_, $name) == 0 } @unames;
+        return if (@m > 1 && !wantarray);
+        return map($userprefix.$self->{NAME}->{$_}->{NAME}, @m) if (@m);
     }
     unless ($user) {
-	@m = grep { index($_, $name) == 0 } @dnames;
-	return if (@m > 1 && !wantarray);
-	return map('-'.$self->{NAME}->{$_}->{NAME}, @m) if (@m);
-	return if (@m > 1);
+        @m = grep { index($_, $name) == 0 } @dnames;
+        return if (@m > 1 && !wantarray);
+        return map('-'.$self->{NAME}->{$_}->{NAME}, @m) if (@m);
+        return if (@m > 1);
     }
 
     # Check for a substring match.
     my $n;
     unless ($disc) {
-	@m = grep { index($_, $name) != -1 } @unames;
-	return if (@m > 1 && !wantarray);
-	# If a user /renamed from a name that's like a discussion,
-	# it may be found in @m.  We don't want that.
-	if (@m && ($m[0] ne $self->{NAME}->{$m[0]}->{NAME})) {
-	    $n = \@m;
-	}
-	elsif (@m) {
-	    return map($userprefix.$self->{NAME}->{$_}->{NAME}, @m);
-	}
+        @m = grep { index($_, $name) != -1 } @unames;
+        return if (@m > 1 && !wantarray);
+        # If a user /renamed from a name that's like a discussion,
+        # it may be found in @m.  We don't want that.
+        if (@m && ($m[0] ne $self->{NAME}->{$m[0]}->{NAME})) {
+            $n = \@m;
+        }
+        elsif (@m) {
+            return map($userprefix.$self->{NAME}->{$_}->{NAME}, @m);
+        }
     }
     unless ($user) {
-	@m = grep { index($_, $name) != -1 } @dnames;
-	return if (@m > 1 && !wantarray);
-	return map('-'.$self->{NAME}->{$_}->{NAME}, @m) if (@m);
+        @m = grep { index($_, $name) != -1 } @dnames;
+        return if (@m > 1 && !wantarray);
+        return map('-'.$self->{NAME}->{$_}->{NAME}, @m) if (@m);
     }
 
     return map($self->{NAME}->{$_}->{NAME}, @$n) if $n;
@@ -479,10 +479,10 @@ sub state {
     # Deal with DATA items.
     # The DATA arg must be set if you want to use these.
     if ($args{DATA}) {
-	if ($args{VALUE}) {
-	    $self->{DATA}{$args{NAME}} = $args{VALUE};      
-	}
-	return $self->{DATA}{$args{NAME}};
+        if ($args{VALUE}) {
+            $self->{DATA}{$args{NAME}} = $args{VALUE};      
+        }
+        return $self->{DATA}{$args{NAME}};
     } 
     
     # OK, the rest of this function refers to the normal records, which
@@ -494,61 +494,61 @@ sub state {
     # figure out if the user is querying or insert/updating.
     my $query = 1;
     foreach (keys %args) {
-	if ( ! /^(HANDLE|NAME)$/ ) {
-	    $query = 0;
-	}
+        if ( ! /^(HANDLE|NAME)$/ ) {
+            $query = 0;
+        }
     }
     
     if ($query) {
-	# ok, it's a query.  return a copy of the record (preferring
-	# the HANDLE index, but using either.
-	if ($args{HANDLE}) {
-	    my $h = $self->{HANDLE}{$args{HANDLE}};
-	    return $h ? %$h : undef;
-	} else {
-	    my $h = $self->{NAME}{lc($args{NAME})};
-	    return $h ? %$h : undef;
-	}
+        # ok, it's a query.  return a copy of the record (preferring
+        # the HANDLE index, but using either.
+        if ($args{HANDLE}) {
+            my $h = $self->{HANDLE}{$args{HANDLE}};
+            return $h ? %$h : undef;
+        } else {
+            my $h = $self->{NAME}{lc($args{NAME})};
+            return $h ? %$h : undef;
+        }
     } else {
-	# OK.  So now we have either an insert or an update.
-	# First check to see if we have a record in the
-	# database (in which case it's an update)
-	
-	my $record;
-	if ($args{HANDLE}) {
-	    $record = $self->{HANDLE}{$args{HANDLE}};
-	} else {
-	    $record = $self->{NAME}{lc($args{NAME})};
-	}
-	
-	if (! ref($record)) {
-	    # create a new record if one was not found.
-	    $record = {};
-	}
+        # OK.  So now we have either an insert or an update.
+        # First check to see if we have a record in the
+        # database (in which case it's an update)
+    
+        my $record;
+        if ($args{HANDLE}) {
+            $record = $self->{HANDLE}{$args{HANDLE}};
+        } else {
+            $record = $self->{NAME}{lc($args{NAME})};
+        }
+    
+        if (! ref($record)) {
+            # create a new record if one was not found.
+            $record = {};
+        }
 
-	# If the handle (unlikely) or name are being updated, remove the
-	# old entry from the indices.  (Note: save_crufty_renames is
-	# intentionally undocumented.)
-	delete $self->{HANDLE}->{$record->{HANDLE}}
-	  if (defined($args{HANDLE}) && !$config{save_crufty_renames});
-	delete $self->{NAME}->{$record->{NAME}} if defined ($args{NAME});
+        # If the handle (unlikely) or name are being updated, remove the
+        # old entry from the indices.  (Note: save_crufty_renames is
+        # intentionally undocumented.)
+        delete $self->{HANDLE}->{$record->{HANDLE}}
+          if (defined($args{HANDLE}) && !$config{save_crufty_renames});
+        delete $self->{NAME}->{$record->{NAME}} if defined ($args{NAME});
 
-	return undef if $args{__DELETE};
+        return undef if $args{__DELETE};
 
-	# OK, now update the record with our arguments.
-	foreach (keys %args) {
-	    $record->{$_}=$args{$_};
-	}
-	
-	# And recreate the indices to make sure things are nice and 
-	# consistent.
-	$self->{HANDLE}{$record->{HANDLE}} = $record
-	  if ($record->{HANDLE});
-	$self->{NAME}{lc($record->{NAME})} = $record
-	  if ($record->{NAME});
-	
-	# and return a copy of the new record.
-	return %{$record};
+        # OK, now update the record with our arguments.
+        foreach (keys %args) {
+            $record->{$_}=$args{$_};
+        }
+    
+        # And recreate the indices to make sure things are nice and 
+        # consistent.
+        $self->{HANDLE}{$record->{HANDLE}} = $record
+          if ($record->{HANDLE});
+        $self->{NAME}{lc($record->{NAME})} = $record
+          if ($record->{NAME});
+    
+        # and return a copy of the new record.
+        return %{$record};
     }
 }
 
@@ -803,14 +803,14 @@ sub store {
                 if ($1) {
                     $args{ui}->print("(Unable to program \"$target:$name\")\n") if ($args{ui});
                     unless (save_deadfile($type, $server, "$target:$name", $text)) {
-	                $args{ui}->print("(Unable to save \"$target:$name\"; changes lost)\n") if ($args{ui});
+                    $args{ui}->print("(Unable to save \"$target:$name\"; changes lost)\n") if ($args{ui});
                     }
                 }
             }
             return 0;
         });
 
-	$args{ui}->print("(Programming \"$target:$name\"\n") if ($args{ui});
+    $args{ui}->print("(Programming \"$target:$name\"\n") if ($args{ui});
         $server->sendln("\@program $target:$name");
         foreach (@{$text}) { chomp; $server->sendln($_) }
         $server->sendln(".");
@@ -820,9 +820,9 @@ sub store {
         $name =~ s/:/ /g if ($type =~ /help/);
 
         if (@$text > 24) {
-	    $args{ui}->print("(Help \"$target $name\" is too long (max 24 lines), saving to deadfile)\n") if ($args{ui});
+            $args{ui}->print("(Help \"$target $name\" is too long (max 24 lines), saving to deadfile)\n") if ($args{ui});
             unless (save_deadfile($type, $server, "$target:$name", $text)) {
-	        $args{ui}->print("(Unable to save \"$target $name\"; changes lost)\n") if ($args{ui});
+                $args{ui}->print("(Unable to save \"$target $name\"; changes lost)\n") if ($args{ui});
             }
             return;
         }
@@ -836,9 +836,9 @@ sub store {
                 $server->sendln(".") unless (@$text == 24);
                 return;
             } elsif ($event->{type} eq 'endcmd' && !$success) {
-	        $args{ui}->print("(Store of help \"$target $name\" failed)\n") if ($args{ui});
+            $args{ui}->print("(Store of help \"$target $name\" failed)\n") if ($args{ui});
                 unless (save_deadfile($type, $server, "$target:$name", $text)) {
-	            $args{ui}->print("(Unable to save \"$target $name\"; changes lost)\n") if ($args{ui});
+                $args{ui}->print("(Unable to save \"$target $name\"; changes lost)\n") if ($args{ui});
                 }
                 return;
             } else {
