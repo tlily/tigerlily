0e9ec0428d00e746118ba1b8593cb5e32f89b439
Will Coleda <will@coleda.com>
2006-05-30 23:29:04 +0000

    coding std cleanup

--- extensions/namethatblurb.pl
+++ extensions/namethatblurb.pl
@@ -10,7 +10,7 @@ my %mention;
 sub blurbupdate {
     my($event, $handler) = @_;
     $event->{server}->state(HANDLE    => $event->{SHANDLE},
-			    BLURBTIME => $event->{TIME});
+                            BLURBTIME => $event->{TIME});
     $mention{$event->{SHANDLE}} = 0;
     return;
 }
@@ -23,30 +23,30 @@ sub blurbcheck {
 
     my @match;
     if ($text =~ /^\'s\s*(?:blurb|\[\])/i) {
-	    for my $rhandle (@{$event->{RHANDLE}}) {
-		    my %attrs = $server->state(HANDLE => $rhandle);
-		    if ($attrs{ATTRIB} && $attrs{ATTRIB} =~ /emote/) {
-			    push @match, $event->{SOURCE};
-			    last;
-		    }
-	    }
+        for my $rhandle (@{$event->{RHANDLE}}) {
+            my %attrs = $server->state(HANDLE => $rhandle);
+            if ($attrs{ATTRIB} && $attrs{ATTRIB} =~ /emote/) {
+                push @match, $event->{SOURCE};
+                last;
+            }
+        }
     } elsif ($text =~ /(.*)\'s\s*(?:blurb|\[\])/i) {
-	    my @name = split /\s+/, $1;
-	    return unless @name;
+        my @name = split /\s+/, $1;
+        return unless @name;
 
-	    my @who;
-	    shift @name while (@name > 3);
-	    while (@name) {
-		    push @who, "@name";
-		    shift @name;
-	    }
+        my @who;
+        shift @name while (@name > 3);
+        while (@name) {
+            push @who, "@name";
+            shift @name;
+        }
 
-	    for (@who) {
-		    @match = grep(!/^-/, $server->expand_name($_));
-		    last if @match;
-	    }
+        for (@who) {
+            @match = grep(!/^-/, $server->expand_name($_));
+            last if @match;
+        }
     } elsif ($text =~ /(my|his|her|its)\s*(?:blurb|\[\])/i) {
-	    @match = ($event->{SOURCE});
+        @match = ($event->{SOURCE});
     }
     return unless @match;
 
@@ -59,24 +59,26 @@ sub blurbcheck {
     $mention{$match[0]->{HANDLE}} = time;
 
     if ($match[0]->{BLURB} eq "") {
-	$ui->print("($match[0]->{NAME} has no blurb)\n");
+        $ui->print("($match[0]->{NAME} has no blurb)\n");
     } else {
-	$ui->print("($match[0]->{NAME}'s blurb is [$match[0]->{BLURB}])\n");
+        $ui->print("($match[0]->{NAME}'s blurb is [$match[0]->{BLURB}])\n");
     }
     return;
 }
 
 sub load {
     event_r(type  => 'private',
-	    order => 'after',
-	    call  => \&blurbcheck);
+        order => 'after',
+        call  => \&blurbcheck);
     event_r(type  => 'public',
-	    order => 'after',
-	    call  => \&blurbcheck);
+        order => 'after',
+        call  => \&blurbcheck);
     event_r(type  => 'emote',
-	    order => 'after',
-	    call  => \&blurbcheck);
+        order => 'after',
+        call  => \&blurbcheck);
     event_r(type  => 'blurb',
-	    order => 'after',
-	    call  => \&blurbupdate);
+        order => 'after',
+        call  => \&blurbupdate);
+
+    return;
 }
