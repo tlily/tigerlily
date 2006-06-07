b7e270d44595182388fffc7e4d6c8c2e7dfe964e
Will Coleda <will@coleda.com>
2006-06-07 20:38:55 +0000

    coding nits

--- extensions/alias.pl
+++ extensions/alias.pl
@@ -2,6 +2,7 @@
 # $Id$
 
 use strict;
+use warnings;
 
 =head1 NAME
 
@@ -22,8 +23,8 @@ my %alias;
 
 sub load {
     event_r(type  => "user_input",
-	    order => "before",
-	    call  => \&aliaser);
+            order => "before",
+            call  => \&aliaser);
     command_r(alias => \&alias_cmd);
 
     shelp_r(alias => "Define client aliases");
@@ -44,7 +45,9 @@ Examples:
 
 %alias hi bob;hi there\\njim;I hate you!
 %alias inbeener /who beener \$*
-});
+    });
+
+    return;
 }
 
 =item %alias
@@ -57,15 +60,15 @@ sub alias_cmd {
     my ($ui,$args) = @_;
 
     if ($args =~ m/^\s*$/ || $args =~ /^\s*list\s*$/) {
-	if (scalar keys %alias) {
-	    $ui->print("The following aliases are defined:\n");
-	    foreach (sort keys %alias) {
-		$ui->print("$_: $alias{$_}\n");
-	    }
-	} else {
-	    $ui->print("(no aliases are currently defined)\n");
-	}
-	return;
+        if (scalar keys %alias) {
+            $ui->print("The following aliases are defined:\n");
+            foreach (sort keys %alias) {
+            $ui->print("$_: $alias{$_}\n");
+            }
+        } else {
+            $ui->print("(no aliases are currently defined)\n");
+        }
+        return;
     }
 
     my ($key,$val) = ($args =~ /^\s*(\w+)\s*(.*?)\s*$/);
@@ -76,11 +79,11 @@ sub alias_cmd {
     }
 
     if ($key eq "clear") {
-	if($val eq "") {
-	    $ui->print("(Usage: %alias clear <command>)\n");
+        if($val eq "") {
+            $ui->print("(Usage: %alias clear <command>)\n");
             return;
-	}
-	delete $alias{$val};
+        }
+        delete $alias{$val};
         $ui->print("(\%$val is now unaliased.)\n");
         return;
     }
@@ -99,6 +102,8 @@ sub alias_cmd {
 
     $alias{$key} = $val;
     $ui->print("(\%$key is now aliased to '$val')\n") if not $config{alias_quiet};
+
+    return;
 }
 
 sub aliaser {
@@ -106,27 +111,28 @@ sub aliaser {
     my $server = active_server();
 
     if ($e->{text} =~ /^%(\S+)\s*(.*)/) {
-	my $newstr = $alias{$1};
-	my $args = $2;
-	my @args = ($1, (split /\s+/,$2));
-	if ($newstr) {
-	    for (0..9) {
-		$newstr =~ s/\$$_/$args[$_]/g;
-	    }
-	    $newstr =~ s/\$\*/$args/g;
-	    if ($newstr =~ /\\n/) {
-		my @rest;
-		($newstr,@rest) = split /\\n/,$newstr;
-		foreach (@rest) {
-		    TLily::Event::send({type => 'user_input',
-					ui   => $e->{ui},
-					text => "$_\n"});
-		}
-	    }
-	    $e->{text} = $newstr;
-	}
+        my $newstr = $alias{$1};
+        my $args = $2;
+        my @args = ($1, (split /\s+/,$2));
+        if ($newstr) {
+            for (0..9) {
+                $newstr =~ s/\$$_/$args[$_]/g;
+            }
+            $newstr =~ s/\$\*/$args/g;
+            if ($newstr =~ /\\n/) {
+                my @rest;
+                ($newstr,@rest) = split /\\n/,$newstr;
+                foreach (@rest) {
+                    TLily::Event::send({type => 'user_input',
+                                        ui   => $e->{ui},
+                                        text => "$_\n"});
+                }
+            }
+            $e->{text} = $newstr;
+        }
     }
 
     return 0;
 }
 
+1;
