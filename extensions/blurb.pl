4eef7eb3bf80916f846d1e9d6a2e5affafb2b892
Will Coleda <will@coleda.com>
2006-06-07 20:26:35 +0000

    coding nits

--- extensions/blurb.pl
+++ extensions/blurb.pl
@@ -2,6 +2,8 @@
 # $Id$
 
 use strict;
+use warnings;
+
 
 # Author: Will "Coke" Coleda (will@coleda.com)
 
@@ -43,16 +45,15 @@ $config{"blurb_verbose"} = 0 if !exists($config{"blurb_verbose"});
 #
 
 my %abbr = (
-	'fou?r' => "4",
-	'ate|eight' => "8",
-	'\b(too?|two)' => "2",
-	'and' => "&",
+    'fou?r' => "4",
+    'ate|eight' => "8",
+    '\b(too?|two)' => "2",
+    'and' => "&",
 );
 
 sub unload {
-	## Nothing to do here right now.
+    ## Nothing to do here right now.
 }
-
 
 my $max = 35;
 my $psuedo;
@@ -62,9 +63,9 @@ my $psuedo;
 #
 sub check_blurb {
         if (length($psuedo) + length($_[0]) <=$max) {
-		return 1;
-	}
-	return 0;
+        return 1;
+    }
+    return 0;
 }
 
 
@@ -72,33 +73,36 @@ sub check_blurb {
 # Go here or away, respecting multicore
 #
 sub away_cmd {
- 	my ($ui, $blurb) = @_;
-	state_cmd($ui,$blurb,"away");
+    my ($ui, $blurb) = @_;
+    state_cmd($ui,$blurb,"away");
+    return;
 }
 
 sub here_cmd {
- 	my ($ui, $blurb) = @_;
-	state_cmd($ui,$blurb,"here");
+    my ($ui, $blurb) = @_;
+    state_cmd($ui,$blurb,"here");
+    return;
 }
 
 sub state_cmd {
- 	my ($ui, $blurb,$state) = @_;
- 	my @servers;
-	if ($config{server_all}) {
-		@servers = TLily::Server::find();
-	} else {
-		$servers[0] = TLily::Server->active();
-	}
+     my ($ui, $blurb,$state) = @_;
+     my @servers;
+    if ($config{server_all}) {
+        @servers = TLily::Server::find();
+    } else {
+        $servers[0] = TLily::Server->active();
+    }
 
-	foreach my $core (@servers) {
-		next if !defined $core;
-		$core->cmd_process("/$state", sub {
-			# I don't see how to get the output of the cmd back...
-		});
-	};
-	if ($blurb && $blurb ne "") {
-		blurb_cmd($ui,$blurb);
-	}
+    foreach my $core (@servers) {
+        next if !defined $core;
+        $core->cmd_process("/$state", sub {
+            # I don't see how to get the output of the cmd back...
+        });
+    };
+    if ($blurb && $blurb ne "") {
+        blurb_cmd($ui,$blurb);
+    }
+    return;
 }
 
 #
@@ -107,123 +111,123 @@ sub state_cmd {
 #
 
 sub blurb_cmd {
-	my ($ui,$blurb) = @_;
-	$psuedo = active_server()->user_name(); #psuedo can change..
-	my $modified = 0;
-	my @words;
-	my $failed=1;
+    my ($ui,$blurb) = @_;
+    $psuedo = active_server()->user_name(); #psuedo can change..
+    my $modified = 0;
+    my @words;
+    my $failed=1;
 
-	if (lc($blurb) eq "off") {
-		my @servers=();
-		if ($config{server_all}) {
-			@servers = TLily::Server::find();
-		} else {
-			$servers[0] = TLily::Server->active();
-		}
-	
-		foreach my $core (@servers) {
-			next if !defined $core;
-			$core->cmd_process("/blurb off", sub {
-				# I don't see how to get the output of the cmd back...
-			});
-		};
-		return;
-	}
+    if (lc($blurb) eq "off") {
+        my @servers=();
+        if ($config{server_all}) {
+            @servers = TLily::Server::find();
+        } else {
+            $servers[0] = TLily::Server->active();
+        }
+    
+        foreach my $core (@servers) {
+            next if !defined $core;
+            $core->cmd_process("/blurb off", sub {
+                # I don't see how to get the output of the cmd back...
+            });
+        };
+        return;
+    }
 
-	## strip off exterior braces/quotes.
+    ## strip off exterior braces/quotes.
 
-	if ($blurb =~ /^"(.*)"$/) {
-		$blurb = $1;
-	} elsif ($blurb =~ /^\[(.*)\]$/) {
-		$blurb = $1;
-	}
+    if ($blurb =~ /^"(.*)"$/) {
+        $blurb = $1;
+    } elsif ($blurb =~ /^\[(.*)\]$/) {
+        $blurb = $1;
+    }
 
-	$ui->print("BRACES/QUOTES:" .$blurb. "\n") if $config{blurb_verbose};
+    $ui->print("BRACES/QUOTES:" .$blurb. "\n") if $config{blurb_verbose};
         goto DONE if (check_blurb($blurb));
 
-	$modified = 1;
+    $modified = 1;
 
-	#Trim any left/right spaces...
+    #Trim any left/right spaces...
 
-	$blurb =~ s/^(\s*)//;
-	$blurb =~ s/(\s*)$//;
+    $blurb =~ s/^(\s*)//;
+    $blurb =~ s/(\s*)$//;
 
-	$ui->print("LEADING/TRAILING SPACE:" .$blurb. "\n") if $config{blurb_verbose};
-        goto DONE if (check_blurb($blurb));
+    $ui->print("LEADING/TRAILING SPACE:" .$blurb. "\n") if $config{blurb_verbose};
+    goto DONE if (check_blurb($blurb));
 
-	#Reduce any multiple spaces to singletons.
+    #Reduce any multiple spaces to singletons.
 
-	$blurb =~ s/(\s+)/ /g;
-	$ui->print("MULTIPLE SPACES:" .$blurb. "\n") if $config{blurb_verbose};
+    $blurb =~ s/(\s+)/ /g;
+    $ui->print("MULTIPLE SPACES:" .$blurb. "\n") if $config{blurb_verbose};
 
-        goto DONE if (check_blurb($blurb));
+    goto DONE if (check_blurb($blurb));
 
-	#Handle any abbreviations;
+    #Handle any abbreviations;
 
-	foreach my $re (keys %abbr) {
-		while ($blurb =~s /$re/$abbr{$re}/i) {
-			$ui->print("ABBR ($re):" .$blurb. "\n") if $config{blurb_verbose};
-        		goto DONE if (check_blurb($blurb));
-		}
-	}
+    foreach my $re (keys %abbr) {
+        while ($blurb =~s /$re/$abbr{$re}/i) {
+            $ui->print("ABBR ($re):" .$blurb. "\n") if $config{blurb_verbose};
+                goto DONE if (check_blurb($blurb));
+        }
+    }
 
-	#Remove all spaces.. convert to a list in the process,
+    #Remove all spaces.. convert to a list in the process,
         #since we need to keep track of words from this point out.
 
-	@words = map {ucfirst $_} (split(' ',$blurb));
-	$blurb = join('',@words);
- 	$ui->print("ALL SPACES:" .$blurb. "\n") if $config{blurb_verbose};
-        goto DONE if (check_blurb($blurb));
+    @words = map {ucfirst $_} (split(' ',$blurb));
+    $blurb = join('',@words);
+    $ui->print("ALL SPACES:" .$blurb. "\n") if $config{blurb_verbose};
+    goto DONE if (check_blurb($blurb));
 
-	#Remove punctuation
+    #Remove punctuation
         my $punc = "'\""; # don't remove &, as it could be there intentionally
 
-	while (grep /$punc/, @words) { #if -any- puncutation
-		foreach my $word (@words) { # remove from each word in turn.
-			if ($word =~ s/$punc//) {
-				$blurb=join('',@words);
-				$ui->print("PUNCTUATION:" .$blurb. "\n") if $config{blurb_verbose};
-        			goto DONE if (check_blurb($blurb));
-			}
-		}
-	}
+    while (grep /$punc/, @words) { #if -any- puncutation
+        foreach my $word (@words) { # remove from each word in turn.
+            if ($word =~ s/$punc//) {
+                $blurb=join('',@words);
+                $ui->print("PUNCTUATION:" .$blurb. "\n") if $config{blurb_verbose};
+                goto DONE if (check_blurb($blurb));
+            }
+        }
+    }
 
-	#Remove some vowels?
+    #Remove some vowels?
 
-	my $vowelRE = '([^AEIOUaeiou])[aeiou]([^AEIOUaeiou])';
+    my $vowelRE = '([^AEIOUaeiou])[aeiou]([^AEIOUaeiou])';
 
-	while (grep /$vowelRE/, @words) { #if -any- cases,
-		foreach my $word (reverse @words) { # remove from each word in turn.
-			if ($word =~ s/$vowelRE/$1$2/) {
-				$blurb=join('',@words);
-				$ui->print("VOWELS:" .$blurb. "\n") if $config{blurb_verbose};
-        			goto DONE if (check_blurb($blurb));
-			}
-		}
-	}
+    while (grep /$vowelRE/, @words) { #if -any- cases,
+        foreach my $word (reverse @words) { # remove from each word in turn.
+            if ($word =~ s/$vowelRE/$1$2/) {
+                $blurb=join('',@words);
+                $ui->print("VOWELS:" .$blurb. "\n") if $config{blurb_verbose};
+                goto DONE if (check_blurb($blurb));
+            }
+        }
+    }
 
-	FAIL:
-        $ui->print("FAIL:\n") if $config{blurb_verbose};
-	$failed=1;
-	$blurb = substr(join('',@words),0,($max-length($psuedo)));
-	$ui->print("(your compressed blurb is is " . abs($max - length($psuedo) - length($blurb)) . " chars too long)\n") if $config{blurb_verbose};
-	#return;
-	
-   	DONE:
-	my @servers=();
-	if ($config{server_all}) {
-		@servers = TLily::Server::find();
-	} else {
-		$servers[0] = TLily::Server->active();
-	}
+    FAIL:
+    $ui->print("FAIL:\n") if $config{blurb_verbose};
+    $failed=1;
+    $blurb = substr(join('',@words),0,($max-length($psuedo)));
+    $ui->print("(your compressed blurb is is " . abs($max - length($psuedo) - length($blurb)) . " chars too long)\n") if $config{blurb_verbose};
+    
+    DONE:
+    my @servers=();
+    if ($config{server_all}) {
+        @servers = TLily::Server::find();
+    } else {
+        $servers[0] = TLily::Server->active();
+    }
 
-	foreach my $core (@servers) {
-		next if !defined $core;
-		$core->cmd_process("/blurb [" . $blurb . "]", sub {
-			# I don't see how to get the output of the cmd back...
-		});
-	};
-	$ui->print("BLURB: " . $blurb . "\n") if $config{blurb_verbose};
+    foreach my $core (@servers) {
+        next if !defined $core;
+        $core->cmd_process("/blurb [" . $blurb . "]", sub {
+            # I don't see how to get the output of the cmd back...
+        });
+    };
+    $ui->print("BLURB: " . $blurb . "\n") if $config{blurb_verbose};
+    return;
 }
 
 #TRUE! They return TRUE!
