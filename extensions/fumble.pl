5ffe76e469501a2acfa508d52a4d43cd59b9dd24
Matthew J. Ryan <mjr@centauri.org>
2007-02-23 20:36:51 +0000

    Finally add fumble.pl after several years of saying, "I should commit fumble.pl."

---
+++ extensions/fumble.pl
@@ -0,0 +1,63 @@
+# -*- Perl -*-
+# $Id$
+
+use strict;
+
+=head1 NAME
+
+fumble.pl - Prevent accidental sends when typing ' or "
+
+=head1 DESCRIPTION
+
+=over 10
+
+=head1 UI COMMANDS
+
+=item toggle-fumble-mode
+
+Toggles fumble mode.
+
+=item fumble-mode
+
+Used internally to intercept each keystroke when fumble mode is enabled,
+and perform the appropriate magic.
+
+=cut
+
+sub fumble_mode {
+    my($ui, $command, $key) = @_;
+
+    if ($key eq "'" || $key eq '"') {
+	$ui->{_eat_nl_flag} = 1;
+        return;
+    } elsif ($key eq "nl" && $ui->{_eat_nl_flag}) {
+	$ui->{_eat_nl_flag} = 0;
+        return 1;
+    } else {
+	$ui->{_eat_nl_flag} = 0;
+        return;
+    }
+}
+
+sub toggle_fumble_mode {
+    my($ui, $command) = @_;
+
+    $ui->{_eat_nl_flag} = 0;
+
+    if ($ui->intercept_u("fumble-mode")) {
+    } elsif ($ui->intercept_r(name => "fumble-mode", order => 990)) {
+    } else {
+        $ui->style("input_error");
+        $ui->print("(cannot start fumble mode)\n");
+        $ui->style("normal");
+    }
+}
+
+TLily::UI::command_r("fumble-mode"        => \&fumble_mode);
+TLily::UI::command_r("toggle-fumble-mode" => \&toggle_fumble_mode);
+
+sub load {
+    my $ui = TLily::UI::name();
+
+    $ui->intercept_r(name => "fumble-mode", order => 990);
+}
