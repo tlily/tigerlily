fd010d219151f090b094b37dd86505ba3f75c483
Will Coleda <will@coleda.com>
2011-06-14 23:45:15 -0400

    don't repeat yourself

--- extensions/cj/rot13.pm
+++ extensions/cj/rot13.pm
@@ -4,15 +4,13 @@ use strict;
 our $TYPE     = "all";
 our $POSITION = 0;
 our $LAST     = 1;
-our $RE       = qr/\brot13\b/i;
+our $RE       = qr/\brot13\s+(.*)/i;
 
 sub response {
     my ($event) = @_;
-    my $args = $event->{VALUE};
-    if ( !( $args =~ s/.*rot13\s+(.*)/$1/i ) ) {
-        return 'ERROR: Expected rot13 RE not matched!';
-    }
 
+    $event->{VALUE} =~ $RE;
+    my $args = $1;
     $args =~ tr/[A-Za-z]/[N-ZA-Mn-za-m]/;
 
     return $args;
