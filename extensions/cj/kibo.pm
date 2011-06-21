a64b3ed1fa89111ac70d92293a6eea89b5a2fa3c
Will Coleda <will@coleda.com>
2011-06-21 15:18:02 -0400

    tidy kibo.

--- extensions/cj/kibo.pm
+++ extensions/cj/kibo.pm
@@ -6,31 +6,32 @@ our $POSITION = 1;
 our $LAST     = 1;
 our $RE       = qr/\b$CJ::name\b.*\?/i,
 
-# some array refs of sayings...
-my $sayings;            # pithy 8ball-isms.
+    # some array refs of sayings...
+    my $sayings;    # pithy 8ball-isms.
 
 # Unify this into generic special handling. =-)
-my $unified;            # special handling for the unified discussion.
-my $beener;             # special handling for the beener discussion.
+my $unified;        # special handling for the unified discussion.
+my $beener;         # special handling for the beener discussion.
 
 sub response {
-        my ($event) = @_;
-        my $list = $sayings;
-        if ( $event->{RECIPS} eq 'unified' ) {
-            $list = [ (@$unified) x 2, @$list ];
-        }
-        elsif ( $event->{RECIPS} eq 'beener' ) {
-            $list = [ (@$beener) x 2, @$list ];
-        }
-        my ($message) = sprintf( CJ::pickRandom($list), $event->{SOURCE} );
-        return $message;
+    my ($event) = @_;
+    my $list = $sayings;
+    if ( $event->{RECIPS} eq 'unified' ) {
+        $list = [ (@$unified) x 2, @$list ];
     }
+    elsif ( $event->{RECIPS} eq 'beener' ) {
+        $list = [ (@$beener) x 2, @$list ];
+    }
+    my ($message) = sprintf( CJ::pickRandom($list), $event->{SOURCE} );
+    return $message;
+}
 
 sub help {
     return "I respond to public questions addressed to me.";
-    TYPE => "public emote",
-    POS  => 1,
-    STOP => 1,
+    TYPE     => "public emote",
+        POS  => 1,
+        STOP => 1,
+        ;
 }
 
 sub load {
