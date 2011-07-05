e83f4f053c0608be8285f95a0709dc652df6e352
Will Coleda <will@coleda.com>
2011-07-05 09:14:33 -0400

    add a -v option to CJ's compute;

--- extensions/CJ/command/compute.pm
+++ extensions/CJ/command/compute.pm
@@ -12,20 +12,25 @@ sub response {
     my ($event) = @_;
     $event->{VALUE} =~ $RE;
 
-    my $args = $1;
+    my $args    = $1;
+    my $verbose = 0;
+    if ( $args =~ s/^-v\s+// ) {
+        $verbose = 1;
+    }
 
     my $url
         = "http://api.wolframalpha.com/v2/query?appid="
         . $CJ::config->val( 'wolfram', 'appID' )
         . "&format=plaintext&input="
-        . escape($1);
+        . escape($args);
 
     CJ::add_throttled_HTTP(
         url      => $url,
         ui_name  => 'main',
         callback => sub {
             my ($response) = @_;
-            CJ::dispatch( $event, scrape_wolfram( $response->{_content} ) );
+            CJ::dispatch( $event,
+                scrape_wolfram( $response->{_content}, $verbose ) );
         }
     );
     return;
@@ -33,13 +38,15 @@ sub response {
 
 sub help {
     return <<END_HELP
-Compute something using WolframAlpha.com
-Usage: compute <stuff>
+Compute something using WolframAlpha.com; If you pass the -v option, I'll
+include a LOT of output from the site, otherwise just the first few lines.
+Usage: compute [-v] <stuff>
 END_HELP
 }
 
 sub scrape_wolfram {
-    my ($content) = shift;
+    my $content = shift;
+    my $verbose = shift;
 
     my $footer = " [wolframalpha.com]";
 
@@ -49,13 +56,21 @@ sub scrape_wolfram {
 
     my $results = "";
 
+    my $maxLines = 2;
+    if ($verbose) {
+        $maxLines = 20;
+    }
+
+    my $line = 0;
     while (
-        $content =~ m/<pod title='(.*?)'.*?<plaintext>(.*?)<\/plaintext>/sig )
+        $content =~ m/<pod title='(.*?)'.*?<plaintext>(.*?)<\/plaintext>/sig
+        && $line < $maxLines )
     {
         my $section   = $1;
         my $plaintext = $2;
         $plaintext =~ s/\n/ /g;
         $results .= "$section: $plaintext\n";
+        $line++;
     }
 
     $results .= $footer;
